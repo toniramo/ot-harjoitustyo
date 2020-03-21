88da12723241de51a96aca24849161c49a3ff068
Toni Rämö <toni.ramo@helsinki.fi>
2020-03-21 12:05:39 +0200

    Lisätään vk2 laskareiden osa 1

--- laskarit/viikko2/Maksukortti/src/test/java/MaksukorttiTest.java
+++ laskarit/viikko2/Maksukortti/src/test/java/MaksukorttiTest.java
@@ -1,3 +1,4 @@
+
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -9,49 +10,85 @@ public class MaksukorttiTest {
 
     public MaksukorttiTest() {
     }
-    
-    Maksukortti kortti; 
-    
+
+    Maksukortti kortti;
+
     @Before
     public void setUp() {
         kortti = new Maksukortti(10);
     }
-     
-     @Test
-     public void konstruktoriAsettaaSaldonOikein() {
-         String vastaus = kortti.toString();
-         assertEquals("Kortilla on rahaa 10.0 euroa", vastaus);
-     }
-     
-     @Test
-     public void syoEdullisestiVahentaaSaldoaOikein() {
-         kortti.syoEdullisesti();
-         assertEquals("Kortilla on rahaa 7.5 euroa", kortti.toString());
-     }
-     
-     @Test
-     public void syoMaukkaastiVahentaaSaldoaOikei() {
-         kortti.syoMaukkaasti();
-         assertEquals("Kortilla on rahaa 6.0 euroa", kortti.toString());
-     }
-     
-     @Test
-     public void syoEdullisestiEiVieSaldoaNegatiiviseksi() {
-         kortti.syoMaukkaasti();
-         kortti.syoMaukkaasti();
-         kortti.syoEdullisesti();
-         assertEquals("Kortilla on rahaa 2.0 euroa", kortti.toString());
-     }
-     
-     @Test
-     public void kortilleVoiLadataRahaa() {
-         kortti.lataaRahaa(25);
-         assertEquals("Kortilla on rahaa 35.0 euroa", kortti.toString());
-     }
-     
-     @Test
-     public void kortinSaldoEiYlitaMaksimiarvoa() {
-         kortti.lataaRahaa(200);
-         assertEquals("Kortilla on rahaa 150.0 euroa", kortti.toString());
-     }
+
+    @Test
+    public void konstruktoriAsettaaSaldonOikein() {
+        String vastaus = kortti.toString();
+        assertEquals("Kortilla on rahaa 10.0 euroa", vastaus);
+    }
+
+    @Test
+    public void syoEdullisestiVahentaaSaldoaOikein() {
+        kortti.syoEdullisesti();
+        assertEquals("Kortilla on rahaa 7.5 euroa", kortti.toString());
+    }
+
+    @Test
+    public void syoMaukkaastiVahentaaSaldoaOikei() {
+        kortti.syoMaukkaasti();
+        assertEquals("Kortilla on rahaa 6.0 euroa", kortti.toString());
+    }
+
+    @Test
+    public void syoEdullisestiEiVieSaldoaNegatiiviseksi() {
+        kortti.syoMaukkaasti();
+        kortti.syoMaukkaasti();
+        kortti.syoEdullisesti();
+        assertEquals("Kortilla on rahaa 2.0 euroa", kortti.toString());
+    }
+
+    @Test
+    public void kortilleVoiLadataRahaa() {
+        kortti.lataaRahaa(25);
+        assertEquals("Kortilla on rahaa 35.0 euroa", kortti.toString());
+    }
+
+    @Test
+    public void kortinSaldoEiYlitaMaksimiarvoa() {
+        kortti.lataaRahaa(200);
+        assertEquals("Kortilla on rahaa 150.0 euroa", kortti.toString());
+    }
+
+    //Harjoituksen 1 lisätestit
+    
+    @Test
+    public void syoMaukkaastiEiVieSaldoaNegatiiviseksi() {
+        kortti.syoMaukkaasti();
+        kortti.syoMaukkaasti();
+        kortti.syoMaukkaasti();
+        assertEquals("Kortilla on rahaa 2.0 euroa", kortti.toString());
+    }
+
+    @Test
+    public void negatiivisenSummanLataaminenEiMuutaSaldoa() {
+        kortti.lataaRahaa(-10);
+        assertEquals("Kortilla on rahaa 10.0 euroa", kortti.toString());
+    }
+
+    @Test
+    public void syoEdullisestiOnnistuuKunRahaaTasanAnnoksenVerran() {
+        kortti.syoEdullisesti();
+        kortti.syoEdullisesti();
+        kortti.syoEdullisesti();
+        //kortilla jäljellä 2.5 euroa
+        kortti.syoEdullisesti();
+        assertEquals("Kortilla on rahaa 0.0 euroa", kortti.toString());
+    }
+    
+    @Test
+    public void syoMaukkaastiOnnistuuKunRahaaTasanAnnoksenVerran() {
+        kortti.syoMaukkaasti();
+        kortti.syoMaukkaasti();
+        kortti.lataaRahaa(2);
+        //kortilla jäljellä 4.0 euroa
+        kortti.syoMaukkaasti();
+        assertEquals("Kortilla on rahaa 0.0 euroa", kortti.toString());
+    }
 }
